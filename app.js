cf5f04ac9ae7128b48b33b2c1a5f55d08044d5b0
SimoneCriscuolo233 <simone.criscuolo26052001@gmail.com>
2025-10-03 18:06:32 +0200

    Add error handling middleware and establish MySQL connection
    - Implemented error handling middleware for centralized error responses.
    - Added not found middleware for handling 404 errors.
    - Established MySQL connection in db.js and logged connection status.
    - Updated app.js to utilize the new middleware and configured dotenv for environment variables.
    - Added dotenv as a dependency in package.json and package-lock.json.

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
-const port = 3000;
-
+const port = process.env.PORT
+const connection = require('./data/db')
 const productRouter = require("./router/productsRouting");
-
+const errorsHandler = require('./middleware/errorsHandler.js')
+const notFound = require('./middleware/notFound.js')
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
-
+app.use(errorsHandler)
+app.use(notFound)
 app.listen(port, () => {
   console.log(`Server in ascolto sulla porta ${port}`);
 });
