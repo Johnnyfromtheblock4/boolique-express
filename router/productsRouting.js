638443fd69d4ce473add35423ce538f252e9f594
rob587 <robertocamm02@yahoo.com>
2025-10-06 10:35:56 +0200

    routing

--- router/productsRouting.js
+++ router/productsRouting.js
@@ -1,3 +1,23 @@
-// tutti i CRUD (index, show, post, put, patch, delete)
+const express = require("express");
+const router = express.Router();
 
-// GENNA
+const productsController = require("../controllers/productsController");
+
+// Rotte CRUD per i prodotti
+
+// GET /products - Elenco di tutti i prodotti
+router.get("/", productsController.getAllProducts);
+
+// GET /products/:id - Ottieni un prodotto specifico
+router.get("/:id", productsController.getProductById);
+
+// POST /products - Crea un nuovo prodotto
+router.post("/", productsController.createProduct);
+
+// PUT /products/:id - Aggiorna un prodotto esistente
+router.put("/:id", productsController.updateProduct);
+
+// DELETE /products/:id - Elimina un prodotto
+router.delete("/:id", productsController.deleteProduct);
+
+module.exports = router;
