1f6d24fb025a2a23740bceb27765cf7b59679d8f
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-10-06 10:15:46 +0200

    ProductsRouting

--- router/productsRouting.js
+++ router/productsRouting.js
@@ -1,3 +1,25 @@
-// tutti i CRUD (index, show, post, put, patch, delete)
+// productsRouting.js
 
-// GENNA
+const express = require('express');
+const router = express.Router();
+
+const productsController = require('../controllers/productsController');
+
+// Rotte CRUD per i prodotti
+
+// GET /products - Elenco di tutti i prodotti
+router.get('/', productsController.getAllProducts);
+
+// GET /products/:id - Ottieni un prodotto specifico
+router.get('/:id', productsController.getProductById);
+
+// POST /products - Crea un nuovo prodotto
+router.post('/', productsController.createProduct);
+
+// PUT /products/:id - Aggiorna un prodotto esistente
+router.put('/:id', productsController.updateProduct);
+
+// DELETE /products/:id - Elimina un prodotto
+router.delete('/:id', productsController.deleteProduct);
+
+module.exports = router;
